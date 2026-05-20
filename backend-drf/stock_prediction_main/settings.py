299e59d703686c198c5b42e037b9c4ae1d7c6550
Manas <manas@Manass-MacBook-Air.local>
2026-05-20 18:19:27 +0100

    Login functionality

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'corsheaders',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,9 @@ STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -6,11 +6,13 @@ import Main from "./components/Main"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Register from "./components/Register"
 import Login from "./components/Login"
+import AuthProvider from './AuthProvider'
 
 function App() {
 
   return (
     <>
+      <AuthProvider>
     <BrowserRouter>
     <Header />
       <Routes>
@@ -20,6 +22,7 @@ function App() {
       </Routes>
     <Footer />
     </BrowserRouter>
+    </AuthProvider>
     </>
   )
 }
